834fc72214de3fb4657979c4f9e9eda7710c615d
Jaya <jaya.dev100x@gmail.com>
2025-06-23 07:26:46 +0530

    button hover submenu

---
+++ components/subMenu/DeveloperSubheader.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DeveloperSubheader = () => {
+  return (
+    <div className='w-130 h-70  bg-white border rounded-xl z-20 pt-10'>
+      
+    </div>
+  )
+}
+
+export default DeveloperSubheader
---
+++ components/subMenu/ResourcesSubheader.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ResourcesSubheader = () => {
+  return (
+    <div className='w-130 h-70 top-13 bg-white border rounded-xl z-20 pt-10'>
+      
+    </div>
+  )
+}
+
+export default ResourcesSubheader
